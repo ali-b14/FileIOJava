93f7667e6fd295ec6108ad0f993aa8ac766f4425
ali-b14 <alib714@gmail.com>
2024-03-12 13:43:12 -0400

    Read through first 3 parts

--- Step4/ReadFour.java
+++ Step4/ReadFour.java
@@ -12,6 +12,23 @@ public class ReadFour
      */
         public static void main(String[] args)
         {
+            try{
+                Scanner sc = new Scanner(new File("input.txt"));
+                while(sc.hasNext()){
+                    String lineIn = sc.nextLine();
+                    String [] input = lineIn.split(" ");
+                    int sum = 0;
+                    for (int i = 0; i < input.length; i++) {
+                        sum += Integer.parseInt(input[i]);
+                    }
+                    System.out.println(sum);
+                }
+            }
+            catch(IOException e){
+                System.out.println("File not found");
+            }
+            System.out.println();
+
             // Print out a running total of all the
             // values in the input file.
         }
